38e78b62b10bf4452c41a0a2c4552c9ec6d97194
8860ErikCampillos <32137949+8860ErikCampillos@users.noreply.github.com>
2017-11-13 20:01:03 +0100

    Add files via upload
    Hoy hemos empezado a crear la página inicial que se verá tras hacer el login

---
+++ mostrar.php
@@ -0,0 +1,39 @@
+<?php
+include("conexion.php");
+//session_start();
+if (is_null($_SESSION['username'])) {
+    header("location: index.php"); /* Redirect browser */
+}
+else {
+
+    $q = "SELECT * FROM recursos";
+    echo "$q";
+    $consulta = mysqli_query($conexion, $q);
+    if (mysqli_num_rows($consulta)>0) {
+        echo "<div class='uno'>";
+        echo "<div class='tres'>"; 
+        while ($recursos=mysqli_fetch_array($consulta)){
+            $foto=$recursos["fotos_recursos"];
+            $boton=$recursos["estado_recursos"];
+            echo "<div class='fichaG'>";
+            echo "<div class='imageG'>";
+            echo "<img src='$foto'/>";
+            echo "</div>";                 
+            echo "</br><h3>". utf8_encode($recursos["nombre_recursos"])."</h3>";
+            echo "<h4>".utf8_encode($recursos["descripcion_recursos"])."</h4>";
+            echo "<button type='button'"." class='btn btn-success'>$boton</button>";
+            echo "</div>";
+        }
+        echo "</div>";
+        echo "</div>";
+
+    } 
+    else{
+        echo "<h1>No hemos encontrado resultados, intente de nuevo en hacer una búsqueda</h1>";
+    }
+
+
+}
+
+?>
+
